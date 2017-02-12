910d3f91b8950ae7efbc7bbcf599135021140ac0
Peter Williams <peter.williams@papercut.com>
2017-02-12 17:57:34 +1100

    added some command line options to gather test statistics

--- pdf/pdf_transform_content_streams.go
+++ pdf/pdf_transform_content_streams.go
@@ -1,7 +1,31 @@
 /*
- * List all content streams for all pages in a pdf file.
+ * "Transform" all content streams in all pages in a list of pdf files.
+
+ * "Transform" is in quotes because this program doesn't perform any transforms. It converts PDF
+ *   files into our internal representation then converts the internal representation back to a PDF
+ *   file and checks that the output PDF file is the same as the input PDF file.
  *
- * Run as: go run pdf_print_content_streams.go input.pdf
+ * Run as: ./pdf_transform_content_streams -o output testdata/*.pdf > blah
+ *
+ *   This will transform all .pdfs file in testdata and write the results to output.
+ *   The main results are written to stderr so you will see them in your console.
+ *	 Detailed information is written to stdout and you will see them in blah.
+ *
+ *  See the other command line options in the top of main()
+ *		-a tests all the input files. The default behaviour is stop at the first failure. Use this
+ *			to find out how many of your corpus files this program works for.
+ *		-x will transform without parsing content streams. Use this to see which failures are due to
+ *			problems in the content parsing code.
+ *			Running -a then -a -x will tell you how well this code is performing on your corpus
+ *			and which failures are due to content parsing.
+ *
+ *
+ *	Currently failing files in PETER's corpus of 332 PDF files
+ *		Radon_Transform.pdf
+ *		ESCP-R reference_151008.pdf
+ *		lda.pdf
+ *		BLUEBOOK.pdf
+ *		pdf_hacks.pdf
  */
 
 package main
@@ -11,11 +35,13 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"sort"
 	"strings"
+	"time"
 
 	unicommon "github.com/unidoc/unidoc/common"
 	// unilicense "github.com/unidoc/unidoc/license"
@@ -43,11 +69,15 @@ func initUniDoc(licenseKey string, debug bool) error {
 }
 
 func main() {
-	debug := false
-	outputDir := ""
-	var minSize int64 = -1
-	var maxSize int64 = -1
+	debug := false               // Write debug level info to stdout?
+	noContentTransforms := false // Don't parse stream contents?
+	runAllTests := false         // Don't stop when a PDF file fails to process?
+	outputDir := ""              // Transformed PDFs are written here
+	var minSize int64 = -1       // Minimum size for an input PDF to be processed.
+	var maxSize int64 = -1       // Maximum size for an input PDF to be processed.
 	flag.BoolVar(&debug, "d", false, "Enable debug logging")
+	flag.BoolVar(&noContentTransforms, "x", false, "Don't transform streams")
+	flag.BoolVar(&runAllTests, "a", false, "Run all tests. Don't stop at first failure")
 	flag.StringVar(&outputDir, "o", "", "Output directory")
 	flag.Int64Var(&minSize, "min", -1, "Minimum size of files to process (bytes)")
 	flag.Int64Var(&maxSize, "max", -1, "Maximum size of files to process (bytes)")
@@ -70,85 +100,112 @@ func main() {
 
 	err := initUniDoc("", debug)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
 	err = os.MkdirAll(outputDir, 0777)
 	if err != nil {
 		unicommon.Log.Error("MkdirAll failed. outputDir=%#q err=%v", outputDir, err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("pdfList=%d %#q\n", len(pdfList), pdfList)
+	compDir := makeUniqueDir("compare.pdfs")
+	fmt.Fprintf(os.Stderr, "compDir=%#q\n", compDir)
+	defer os.RemoveAll(compDir)
 
-	for idx, inputPath := range sortFiles(pdfList, minSize, maxSize) {
+	pdfList = sortFiles(pdfList, minSize, maxSize)
+	fmt.Printf("pdfList=%d %#q\n", len(pdfList), pdfList)
+	badFiles := []string{}
+	failFiles := []string{}
+
+	for idx, inputPath := range pdfList {
 		_, name := filepath.Split(inputPath)
 		inputSize := fileSize(inputPath)
 		fmt.Fprintf(os.Stderr, "inputPath %3d of %d %#-30q  (%6d->", idx,
 			len(pdfList), name, inputSize)
 		outputPath := modifyPath(inputPath, outputDir)
-		err = transformContentStreams(inputPath, outputPath)
 
-		outputSize := fileSize(outputPath)
-		fmt.Fprintf(os.Stderr, "%6d %3d%%) => %#q\n",
-			outputSize, int(float64(outputSize)/float64(inputSize)*100.0+0.5), outputPath)
+		t0 := time.Now()
+		numPages, err := transformContentStreams(inputPath, outputPath, noContentTransforms)
+		dt := time.Since(t0)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "transformContentStreams failed. err=%v\n", err)
+			unicommon.Log.Error("transformContentStreams failed. err=%v", err)
+			failFiles = append(failFiles, inputPath)
+			if runAllTests {
+				continue
+			}
 			os.Exit(1)
 		}
 
-		equal, err := pdfsEqual(inputPath, outputPath, "compare.pdfs")
+		outputSize := fileSize(outputPath)
+		fmt.Fprintf(os.Stderr, "%6d %3d%%) %d pages %.3f sec => %#q\n",
+			outputSize, int(float64(outputSize)/float64(inputSize)*100.0+0.5),
+			numPages, dt.Seconds(), outputPath)
+
+		equal, badInput, err := pdfsEqual(inputPath, outputPath, compDir)
+		if badInput {
+			unicommon.Log.Error("Bad input PDF. inputPath=%#q err=%v", inputPath, err)
+			badFiles = append(badFiles, inputPath)
+			continue
+		}
 		if !equal || err != nil {
 			unicommon.Log.Error("Transform has changed PDF. inputPath=%q outputPath=%q",
 				inputPath, outputPath)
-			return
+			failFiles = append(failFiles, inputPath)
+			if runAllTests {
+				continue
+			}
+			os.Exit(1)
 		}
 	}
 
-	fmt.Printf("%d files\n", len(pdfList))
+	fmt.Fprintf(os.Stderr, "%d files %d bad %d failed\n",
+		len(pdfList), len(badFiles), len(failFiles))
+	fmt.Fprintf(os.Stderr, "%d bad\n", len(badFiles))
+	for i, path := range badFiles {
+		fmt.Fprintf(os.Stderr, "%d %#q\n", i, path)
+	}
+	fmt.Fprintf(os.Stderr, "%d fail\n", len(failFiles))
+	for i, path := range failFiles {
+		fmt.Fprintf(os.Stderr, "f%d %#q\n", i, path)
+	}
+
 	fmt.Printf("%d ops ---------^^^----------------\n", len(allOpCounts))
 	for i, k := range sortCounts(allOpCounts) {
-		fmt.Printf("%3d: %6q %5d\n", i, k, allOpCounts[k])
+		fmt.Printf("%3d: %6#q %5d\n", i, k, allOpCounts[k])
 	}
+
 }
 
 var allOpCounts = map[string]int{}
 
-func transformContentStreams(inputPath, outputPath string) error {
+func transformContentStreams(inputPath, outputPath string, noContentTransforms bool) (int, error) {
 	f, err := os.Open(inputPath)
 	if err != nil {
-		return err
+		return 0, err
 	}
-
 	defer f.Close()
 
 	pdfReader, err := unipdf.NewPdfReader(f)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	isEncrypted, err := pdfReader.IsEncrypted()
 	if err != nil {
-		return err
+		return 0, err
 	}
-
 	if isEncrypted {
 		_, err = pdfReader.Decrypt([]byte(""))
 		if err != nil {
-			return err
+			return 0, err
 		}
 	}
 
 	numPages, err := pdfReader.GetNumPages()
 	if err != nil {
-		return err
+		return numPages, err
 	}
-
-	fmt.Println("--------------------")
-	fmt.Println("Content streams:")
-	fmt.Println("--------------------")
-
-	opCounts := map[string]int{}
 
 	pdfWriter := unipdf.NewPdfWriter()
 
@@ -156,79 +213,77 @@ func transformContentStreams(inputPath, outputPath string) error {
 		pageNum := i + 1
 		page := pdfReader.PageList[i]
 
-		cstream, err := page.GetAllContentStreams()
-		if err != nil {
-			return err
-		}
-
-		cstreamParser := unipdf.NewContentStreamParser(cstream, page)
-		unicommon.Log.Debug("transformContentStream: pageNum=%d cstream=\n'%s'\nXXXXXX",
-			pageNum, cstream)
-		operations, err := cstreamParser.Parse()
-		if err != nil {
-			return err
-		}
-
-		opStrings := []string{}
-		for _, op := range operations {
-			// fmt.Printf("Operation %d: %s - Params: %+v\n", idx+1, op.Operand, op.Params)
-			opCounts[op.Operand]++
-			allOpCounts[op.Operand]++
-			s := op.DefaultWriteString()
-			// s = fmt.Sprintf("\t%-40s %% command %d of %d [%d] %s\n", s, i, len(operations),
-			// 	len(op.Params), op.Operand)
-
-			opStrings = append(opStrings, s)
-		}
-
-		cstreamOut := strings.Join(opStrings, " ")
-
-		fmt.Printf("Page %d - content stream %d: %d => %d\n", pageNum, len(cstream), len(cstreamOut))
-
-		// for name, ximg := range xobjDict {
-		// 	unicommon.Log.Debug("transformContentStreams: name=%#q ximg=%T", name, ximg)
-		// 	err = page.AddImageResource(name, ximg)
-		// 	if err != nil {
-		// 		return err
-		// 	}
-		// }
-
-		err = page.SetContentStreams([]string{cstreamOut})
-		if err != nil {
-			return err
+		if !noContentTransforms {
+			err = transformPageContents(page, pageNum)
+			if err != nil {
+				return numPages, err
+			}
 		}
 
 		err = pdfWriter.AddPage(page)
 		if err != nil {
-			return err
+			return numPages, err
 		}
 	}
 
-	pageDict, err := pdfWriter.Pages()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%d pages -----------@@@-----------\n", len(*pageDict))
-	fmt.Printf("%s\n", *pageDict)
-
 	fWrite, err := os.Create(outputPath)
+	if err != nil {
+		return numPages, err
+	}
+	defer fWrite.Close()
+
+	err = pdfWriter.Write(fWrite)
+
+	return numPages, nil
+}
+
+func transformPageContents(page *unipdf.PdfPage, pageNum int) error {
+	// fmt.Fprintln(os.Stderr, "$$$")
+
+	opCounts := map[string]int{}
+
+	cstream, err := page.GetAllContentStreams()
 	if err != nil {
 		return err
 	}
 
-	defer fWrite.Close()
+	cstreamParser := unipdf.NewContentStreamParser(cstream, page)
+	unicommon.Log.Debug("transformContentStream: pageNum=%d cstream=\n'%s'\nXXXXXX",
+		pageNum, cstream)
+	operations, err := cstreamParser.Parse()
+	if err != nil {
+		return err
+	}
 
-	err = pdfWriter.Write(fWrite)
+	opStrings := []string{}
+	for _, op := range operations {
+		opCounts[op.Operand]++
+		allOpCounts[op.Operand]++
+		s := op.DefaultWriteString()
+		opStrings = append(opStrings, s)
+	}
+
+	cstreamOut := strings.Join(opStrings, " ")
+
+	fmt.Printf("Page %d - content stream %d: %d => %d\n", pageNum, len(cstream), len(cstreamOut))
+
+	// for name, ximg := range xobjDict {
+	// 	unicommon.Log.Debug("transformContentStreams: name=%#q ximg=%T", name, ximg)
+	// 	err = page.AddImageResource(name, ximg)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	err = page.SetContentStreams([]string{cstreamOut})
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("%d ops -------------------------\n", len(opCounts))
 	for i, k := range sortCounts(opCounts) {
-		fmt.Printf("%3d: %6q %5d\n", i, k, opCounts[k])
+		fmt.Printf("%3d: %6#q %5d\n", i, k, opCounts[k])
 	}
-
 	return nil
 }
 
@@ -273,7 +328,7 @@ func (x byCount) Less(i, j int) bool {
 
 func modifyPath(inputPath, outputDir string) string {
 	_, name := filepath.Split(inputPath)
-	name = fmt.Sprintf("%08d_%s", fileSize(inputPath), name)
+	// name = fmt.Sprintf("%08d_%s", fileSize(inputPath), name)
 
 	outputPath := filepath.Join(outputDir, name)
 	in, err := filepath.Abs(inputPath)
@@ -287,6 +342,7 @@ func modifyPath(inputPath, outputDir string) string {
 	if strings.ToLower(in) == strings.ToLower(out) {
 		unicommon.Log.Error("modifyPath: Cannot modify path to itself. inputPath=%#q outputDir=%#q",
 			inputPath, outputDir)
+		panic("Don't write over test files")
 	}
 	return outputPath
 }
@@ -312,12 +368,17 @@ func sortFiles(pathList []string, minSize, maxSize int64) []string {
 	if maxSize >= 0 {
 		i1 = sort.Search(len(fdList), func(i int) bool { return fdList[i].Size() >= maxSize })
 	}
+	fmt.Printf("fdList=%d\n", len(fdList))
+
 	fdList = fdList[i0:i1]
 
-	outList := make([]string, n)
+	fmt.Printf("fdList=%d\n", len(fdList))
+
+	outList := make([]string, len(fdList))
 	for i, fd := range fdList {
 		outList[i] = fd.path
 	}
+	fmt.Printf("outList=%d\n", len(outList))
 
 	return outList
 }
@@ -327,8 +388,7 @@ type FileData struct {
 	os.FileInfo
 }
 
-// byName sorts slices of PdfObjectName. It is needed because sort.Strings(keys) gives a typecheck
-// error, which I find strange because a PdfObjectName is a string.
+// byFile sorts slices of FileData by some file attribute, currently size.
 type byFile []FileData
 
 func (x byFile) Len() int { return len(x) }
@@ -343,38 +403,46 @@ func (x byFile) Less(i, j int) bool {
 	return x[i].path < x[j].path
 }
 
-func pdfsEqual(path1, path2, temp string) (bool, error) {
+// pdfsEqual compares PDF files `path1` and `path2` and returns equal, bad1, err where
+// 	equal: PDF files are equal
+//  bad1: error is in path1
+//  err: the error
+
+func pdfsEqual(path1, path2, temp string) (bool, bool, error) {
 	dir1 := filepath.Join(temp, "1")
 	dir2 := filepath.Join(temp, "2")
 	err := os.MkdirAll(dir1, 0777)
 	if err != nil {
 		panic(err)
 	}
+	defer os.RemoveAll(dir1)
 	err = os.MkdirAll(dir2, 0777)
 	if err != nil {
 		panic(err)
 	}
+	defer os.RemoveAll(dir2)
+
 	err = runGhostscript(path1, dir1)
 	if err != nil {
-		panic(err)
+		return false, true, err
 	}
 	err = runGhostscript(path2, dir2)
 	if err != nil {
-		panic(err)
+		return false, false, err
 	}
+
 	equal, err := directoriesEqual(dir1, dir2, temp)
 	if err != nil {
 		panic(err)
 	}
-	os.RemoveAll(dir1)
-	os.RemoveAll(dir2)
-	return equal, nil
+
+	return equal, false, nil
 }
 
 // runPs2Pdf runs Ghostscript  on file `pdf` to create file one png file per page in directory
 // `outputDir`
 func runGhostscript(pdf, outputDir string) error {
-	unicommon.Log.Info("runGhostscript: pdf=%#q outputDir=%#q", pdf, outputDir)
+	unicommon.Log.Debug("runGhostscript: pdf=%#q outputDir=%#q", pdf, outputDir)
 	outputPath := filepath.Join(outputDir, "doc-%03d.png")
 	output := fmt.Sprintf("-sOutputFile=%s", outputPath)
 	cmd := exec.Command("gs",
@@ -386,7 +454,7 @@ func runGhostscript(pdf, outputDir string) error {
 		"-dTextAlphaBits=4",
 		output,
 		pdf)
-	unicommon.Log.Info("runGhostscript: cmd=%#q", cmd.Args)
+	unicommon.Log.Debug("runGhostscript: cmd=%#q", cmd.Args)
 
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
@@ -438,4 +506,21 @@ func filesEqual(path1, path2 string) (bool, error) {
 		panic(err)
 	}
 	return bytes.Equal(f1, f2), nil
+}
+
+func makeUniqueDir(baseDir string) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 1000; i++ {
+		dir := filepath.Join(baseDir, fmt.Sprintf("dir.%03d", i))
+		if _, err := os.Stat(dir); err != nil {
+			if os.IsNotExist(err) {
+				if err := os.MkdirAll(dir, 0777); err != nil {
+					panic(err)
+				}
+				return dir
+			}
+		}
+		time.Sleep(time.Duration(r.Float64() * float64(time.Second)))
+	}
+	panic("Cannot create new directory")
 }
