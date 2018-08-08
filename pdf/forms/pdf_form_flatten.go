e50edabaaf278f468f3dee65bf1908f448e4cc2b
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2018-08-08 13:19:18 +0000

    Clean up flattening example

--- pdf/forms/pdf_form_flatten.go
+++ pdf/forms/pdf_form_flatten.go
@@ -1,9 +1,8 @@
 /*
- * Flatten form data in a PDF file, move to content stream, so cannot be edited.
- * Note: Currently only works for field data with an apperance stream.
- * TODO: Add support for generating appearance streams (default for fields missing AP or force generation on all).
+ * Flatten form data in PDF files, moving to content stream from annotations, so cannot be edited.
+ * Note: Works for forms that have been filled in an editor and have the appearance streams generated.
  *
- * Run as: go run pdf_form_flatten.go <input.pdf> <flattened.pdf>
+ * Run as: go run pdf_form_flatten.go <outputdir> <pdf files...>
  */
 
 package main
@@ -11,22 +10,18 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"path/filepath"
 	"sort"
 
-	unicommon "github.com/unidoc/unidoc/common"
 	"github.com/unidoc/unidoc/pdf/model"
 )
 
 func main() {
 	// When debugging, enable debug-level logging via console:
-	unicommon.SetLogger(unicommon.NewConsoleLogger(unicommon.LogLevelDebug))
+	//unicommon.SetLogger(unicommon.NewConsoleLogger(unicommon.LogLevelDebug))
 
 	if len(os.Args) < 3 {
-		//fmt.Printf("Usage: go run pdf_form_flatten.go <input.pdf> <output.pdf>\n")
-		fmt.Printf("Usage: go run pdf_form_flatten.go <outputdir> <input1.pdf> ...\n")
-
+		fmt.Printf("Usage: go run pdf_form_flatten.go <outputdir> <input1.pdf> [input2.pdf] ...\n")
 		os.Exit(1)
 	}
 
@@ -58,8 +53,6 @@ func main() {
 
 // flattenPdf flattens annotations and forms moving the appearance stream to the page contents so cannot be
 // modified.
-// TODO: Cross-reference between annotations and form fields, should give an error if a form field does not
-// have an appearance stream (widget annotation) present for drawing it.
 func flattenPdf(inputPath, outputPath string) error {
 	f, err := os.Open(inputPath)
 	if err != nil {
@@ -97,91 +90,3 @@ func flattenPdf(inputPath, outputPath string) error {
 	err = pdfWriter.Write(fout)
 	return err
 }
-
-/*
-// flatten flattens the PDF loaded in `pdf`. If `doannots` is true, all annotations will be flattened.
-// If `dofields` is true will lookup all widget annotations corresponding to form fields and flatten them.
-//
-// References to flattened annotations will be removed from Page Annots array. For fields the AcroForm entry
-// will be emptied.
-//
-// TODO: Plan is to add Flatten(dofields, doannots bool) error to PdfReader.
-func flatten(pdf *model.PdfReader, dofields, doannots bool) error {
-
-}
-
-// Get the active XObject Form for an appearance dictionary.
-// Default gets the N entry, and if it is a dictionary, picks the entry referred to by AS.
-// If returned XObject Form is nil (and no errors) it indicates that the annotation has no appearance.
-func getAnnotationActiveAppearance(annot *model.PdfAnnotation) (*model.XObjectForm, *model.PdfRectangle, error) {
-
-	fmt.Printf("----\n")
-	fmt.Printf("annot: %#v\n", annot)
-	fmt.Printf("context: %#v\n", annot.GetContext())
-	fmt.Printf("obj: %v\n", annot.GetContainingPdfObject())
-
-	// Appearance dictionary entries (Table 168 p. 397).
-	apDict, has := core.GetDict(annot.AP)
-	if !has {
-		return nil, nil, errors.New("field missing AP dictionary")
-	}
-
-	// Get the Rect specifying the display rectangle.
-	rectArr, has := core.GetArray(annot.Rect)
-	if !has || rectArr.Len() != 4 {
-		return nil, nil, errors.New("rect invalid")
-	}
-	rect, err := model.NewPdfRectangle(*rectArr)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	nobj := core.TraceToDirectObject(apDict.Get("N"))
-	switch t := nobj.(type) {
-	case *core.PdfObjectStream:
-		stream := t
-		xform, err := model.NewXObjectFormFromStream(stream)
-		return xform, rect, err
-	case *core.PdfObjectDictionary:
-		// An annotation representing multiple fields may have many appearances.
-		// As an example checkbox may have two appearance states On and Off.
-		// Its appearance dictionary would contain /N << /On Ref /Off Ref >>, the choice is
-		// determines by the AS entry in the annotation dictionary.
-		nDict := t
-
-		fmt.Printf("AS: %v\n", annot.AS)
-		state, has := core.GetName(annot.AS)
-		if !has {
-			//state = core.MakeName("Off") // Default.
-			// No appearance (nil).
-			return nil, nil, nil
-		}
-
-		// If only 1 element, use it.
-		// XXX/FIXME: Not always good behavior? What about V?
-		/
-			if len(nDict.Keys()) == 1 {
-				state = &nDict.Keys()[0]
-			}
-			fmt.Printf("State: '%s'\n", state.String())
-		/
-
-		if nDict.Get(*state) == nil {
-			fmt.Printf("Error: AS state not specified in AP dict\n")
-			return nil, nil, nil
-		}
-
-		stream, has := core.GetStream(nDict.Get(*state))
-		if !has {
-			fmt.Printf("Unable to access stream for %v\n", state)
-			return nil, nil, errors.New("stream missing")
-		}
-		xform, err := model.NewXObjectFormFromStream(stream)
-		return xform, rect, err
-	}
-
-	fmt.Printf("Invalid type for N: %T\n", nobj)
-	return nil, nil, errors.New("type check error")
-}
-
-*/
