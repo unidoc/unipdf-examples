4366cd9d61e9d2b1d70fa2940c104b34f1d489ce
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2019-05-25 11:15:33 +0000

    update image example to use extractor (#47)

--- image/pdf_extract_images.go
+++ image/pdf_extract_images.go
@@ -14,13 +14,10 @@ import (
 	"image/jpeg"
 	"os"
 
-	pdfcontent "github.com/unidoc/unipdf/v3/contentstream"
-	pdfcore "github.com/unidoc/unipdf/v3/core"
-	pdf "github.com/unidoc/unipdf/v3/model"
+	"github.com/unidoc/unipdf/v3/extractor"
+	"github.com/unidoc/unipdf/v3/model"
 )
 
-var xObjectImages = 0
-var inlineImages = 0
 
 func main() {
 	// Enable debug-level console logging, when debuggingn:
@@ -40,11 +37,6 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
-
-	fmt.Printf("-- Summary\n")
-	fmt.Printf("%d XObject images extracted\n", xObjectImages)
-	fmt.Printf("%d inline images extracted\n", inlineImages)
-	fmt.Printf("Total %d images\n", xObjectImages+inlineImages)
 }
 
 // Extracts images and properties of a PDF specified by inputPath.
@@ -57,7 +49,7 @@ func extractImagesToArchive(inputPath, outputPath string) error {
 
 	defer f.Close()
 
-	pdfReader, err := pdf.NewPdfReader(f)
+	pdfReader, err := model.NewPdfReader(f)
 	if err != nil {
 		return err
 	}
@@ -95,6 +87,7 @@ func extractImagesToArchive(inputPath, outputPath string) error {
 	defer zipf.Close()
 	zipw := zip.NewWriter(zipf)
 
+	totalImages := 0
 	for i := 0; i < numPages; i++ {
 		fmt.Printf("-----\nPage %d:\n", i+1)
 
@@ -103,17 +96,24 @@ func extractImagesToArchive(inputPath, outputPath string) error {
 			return err
 		}
 
-		// List images on the page.
-		rgbImages, err := extractImagesOnPage(page)
+
+		pextract, err := extractor.New(page)
 		if err != nil {
 			return err
 		}
-		_ = rgbImages
 
-		for idx, img := range rgbImages {
+		pimages, err := pextract.ExtractPageImages(nil)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%d Images\n", len(pimages.Images))
+		for idx, img := range pimages.Images {
+			fmt.Printf("Image %d - X: %.2f Y: %.2f, Width: %.2f, Height: %.2f\n",
+				totalImages+idx+1, img.X, img.Y, img.Width, img.Height)
 			fname := fmt.Sprintf("p%d_%d.jpg", i+1, idx)
 
-			gimg, err := img.ToGoImage()
+			gimg, err := img.Image.ToGoImage()
 			if err != nil {
 				return err
 			}
@@ -128,7 +128,9 @@ func extractImagesToArchive(inputPath, outputPath string) error {
 				return err
 			}
 		}
+		totalImages += len(pimages.Images)
 	}
+	fmt.Printf("Total: %d images\n", totalImages)
 
 	// Make sure to check the error on Close.
 	err = zipw.Close()
@@ -139,115 +141,3 @@ func extractImagesToArchive(inputPath, outputPath string) error {
 	return nil
 }
 
-func extractImagesOnPage(page *pdf.PdfPage) ([]*pdf.Image, error) {
-	contents, err := page.GetAllContentStreams()
-	if err != nil {
-		return nil, err
-	}
-
-	return extractImagesInContentStream(contents, page.Resources)
-}
-
-func extractImagesInContentStream(contents string, resources *pdf.PdfPageResources) ([]*pdf.Image, error) {
-	rgbImages := []*pdf.Image{}
-	cstreamParser := pdfcontent.NewContentStreamParser(contents)
-	operations, err := cstreamParser.Parse()
-	if err != nil {
-		return nil, err
-	}
-
-	processedXObjects := map[string]bool{}
-
-	// Range through all the content stream operations.
-	for _, op := range *operations {
-		if op.Operand == "BI" && len(op.Params) == 1 {
-			// BI: Inline image.
-
-			iimg, ok := op.Params[0].(*pdfcontent.ContentStreamInlineImage)
-			if !ok {
-				continue
-			}
-
-			img, err := iimg.ToImage(resources)
-			if err != nil {
-				return nil, err
-			}
-
-			cs, err := iimg.GetColorSpace(resources)
-			if err != nil {
-				return nil, err
-			}
-			if cs == nil {
-				// Default if not specified?
-				cs = pdf.NewPdfColorspaceDeviceGray()
-			}
-			fmt.Printf("Cs: %T\n", cs)
-
-			rgbImg, err := cs.ImageToRGB(*img)
-			if err != nil {
-				return nil, err
-			}
-
-			rgbImages = append(rgbImages, &rgbImg)
-			inlineImages++
-		} else if op.Operand == "Do" && len(op.Params) == 1 {
-			// Do: XObject.
-			name := op.Params[0].(*pdfcore.PdfObjectName)
-
-			// Only process each one once.
-			_, has := processedXObjects[string(*name)]
-			if has {
-				continue
-			}
-			processedXObjects[string(*name)] = true
-
-			_, xtype := resources.GetXObjectByName(*name)
-			if xtype == pdf.XObjectTypeImage {
-				fmt.Printf(" XObject Image: %s\n", *name)
-
-				ximg, err := resources.GetXObjectImageByName(*name)
-				if err != nil {
-					return nil, err
-				}
-
-				img, err := ximg.ToImage()
-				if err != nil {
-					return nil, err
-				}
-
-				rgbImg, err := ximg.ColorSpace.ImageToRGB(*img)
-				if err != nil {
-					return nil, err
-				}
-				rgbImages = append(rgbImages, &rgbImg)
-				xObjectImages++
-			} else if xtype == pdf.XObjectTypeForm {
-				// Go through the XObject Form content stream.
-				xform, err := resources.GetXObjectFormByName(*name)
-				if err != nil {
-					return nil, err
-				}
-
-				formContent, err := xform.GetContentStream()
-				if err != nil {
-					return nil, err
-				}
-
-				// Process the content stream in the Form object too:
-				formResources := xform.Resources
-				if formResources == nil {
-					formResources = resources
-				}
-
-				// Process the content stream in the Form object too:
-				formRgbImages, err := extractImagesInContentStream(string(formContent), formResources)
-				if err != nil {
-					return nil, err
-				}
-				rgbImages = append(rgbImages, formRgbImages...)
-			}
-		}
-	}
-
-	return rgbImages, nil
-}
