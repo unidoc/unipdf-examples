22c839c1a1f5e20d61045e4a282f075d451c4ba0
Peter Williams <peter.williams@papercut.com>
2019-11-29 16:11:45 +1100

    Improve compression of ICC color images

--- compress/pdf_optimize.go
+++ compress/pdf_optimize.go
@@ -6,11 +6,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/unidoc/unipdf/v3/common"
+	"github.com/unidoc/unipdf/v3/contentstream"
+	"github.com/unidoc/unipdf/v3/core"
 	"github.com/unidoc/unipdf/v3/model"
 	"github.com/unidoc/unipdf/v3/model/optimize"
 )
@@ -18,51 +22,82 @@ import (
 const usage = "Usage: %s INPUT_PDF_PATH OUTPUT_PDF_PATH\n"
 
 func main() {
-	args := os.Args
-	if len(args) < 3 {
-		fmt.Printf(usage, os.Args[0])
-		return
-	}
-	inputPath := args[1]
-	outputPath := args[2]
+	var debug, trace bool
+	var simpleColor bool
+	flag.BoolVar(&debug, "d", false, "Print debugging information.")
+	flag.BoolVar(&trace, "e", false, "Print detailed debugging information.")
+	flag.BoolVar(&simpleColor, "c", false, "Convert ICC color images to grayscale or RGB.")
+	makeUsage(usage)
 
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if trace {
+		common.SetLogger(common.NewConsoleLogger(common.LogLevelTrace))
+	} else if debug {
+		common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
+	} else {
+		common.SetLogger(common.NewConsoleLogger(common.LogLevelInfo))
+	}
+
+	inputPath := args[0]
+	outputPath := args[1]
+	err := optimizePDF(inputPath, outputPath, simpleColor)
+	if err != nil {
+		log.Fatal("Fail: %v\n", err)
+	}
+}
+
+// optimizePDF reduces the size of PDF `inputPath` and writes the result to `outputPath`. If
+// simpleColor is true, ICC color images are converted to DeviceGray or DeviceRGB.
+func optimizePDF(inputPath, outputPath string, simpleColor bool) error {
 	// Initialize starting time.
 	start := time.Now()
 
 	// Get input file stat.
 	inputFileInfo, err := os.Stat(inputPath)
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 
 	// Create reader.
 	inputFile, err := os.Open(inputPath)
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 	defer inputFile.Close()
 
 	reader, err := model.NewPdfReader(inputFile)
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 
 	// Get number of pages in the input file.
-	pages, err := reader.GetNumPages()
+	numPages, err := reader.GetNumPages()
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 
 	// Add input file pages to the writer.
 	writer := model.NewPdfWriter()
-	for i := 1; i <= pages; i++ {
-		page, err := reader.GetPage(i)
+	for pageNum := 1; pageNum <= numPages; pageNum++ {
+		common.Log.Debug("page %d", pageNum)
+		page, err := reader.GetPage(pageNum)
 		if err != nil {
-			log.Fatal("Fail: %v\n", err)
+			return err
 		}
-
+		if simpleColor {
+			err = convertPageToSimpleColor(page)
+			if err != nil {
+				return err
+			}
+		}
 		if err = writer.AddPage(page); err != nil {
-			log.Fatal("Fail: %v\n", err)
+			return err
 		}
 	}
 
@@ -85,32 +120,158 @@ func main() {
 	// Create output file.
 	outputFile, err := os.Create(outputPath)
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 	defer outputFile.Close()
 
 	// Write output file.
 	err = writer.Write(outputFile)
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 
 	// Get output file stat.
 	outputFileInfo, err := os.Stat(outputPath)
 	if err != nil {
-		log.Fatal("Fail: %v\n", err)
+		return err
 	}
 
 	// Print basic optimization statistics.
 	inputSize := inputFileInfo.Size()
 	outputSize := outputFileInfo.Size()
-	ratio := 100.0 - (float64(outputSize) / float64(inputSize) * 100.0)
-	duration := float64(time.Since(start)) / float64(time.Millisecond)
+	percentage := float64(outputSize) / float64(inputSize) * 100.0
+	ratio := 100.0 - percentage
+	duration := time.Since(start).Seconds()
 
-	fmt.Printf("Original file: %s\n", inputPath)
-	fmt.Printf("Original size: %d bytes\n", inputSize)
+	fmt.Printf(" Original file: %s\n", inputPath)
 	fmt.Printf("Optimized file: %s\n", outputPath)
-	fmt.Printf("Optimized size: %d bytes\n", outputSize)
-	fmt.Printf("Compression ratio: %.2f%%\n", ratio)
-	fmt.Printf("Processing time: %.2f ms\n", duration)
+	fmt.Printf(" Original size: %d bytes = %5.2f MB\n", inputSize, mb(inputSize))
+	fmt.Printf("Optimized size: %d bytes = %5.2f MB\n", outputSize, mb(outputSize))
+	fmt.Printf("Compression ratio: %.2f%% = %.2f%% of original\n", ratio, percentage)
+	fmt.Printf("Processing time: %.3f sec\n", duration)
+
+	return nil
+}
+
+// convertPageToSimpleColor goes through all XObject images referenced by `page` and converts 1 and
+// 3 component ICC images to RGB to grayscale and RGB respectively.
+func convertPageToSimpleColor(page *model.PdfPage) error {
+	contents, err := page.GetAllContentStreams()
+	if err != nil {
+		return err
+	}
+	return transformImagesToSimpleColor(contents, page.Resources)
+}
+
+// convertPageToSimpleColor goes through all XObject images in `contents` and converts 1 and 3
+// component ICC images to grayscale and RGB respectively.
+func transformImagesToSimpleColor(contents string, resources *model.PdfPageResources) error {
+	cstreamParser := contentstream.NewContentStreamParser(contents)
+	operations, err := cstreamParser.Parse()
+	if err != nil {
+		return err
+	}
+
+	// Keep track of processed XObjects to avoid repetition.
+	processedXObjects := map[string]struct{}{}
+
+	processor := contentstream.NewContentStreamProcessor(*operations)
+	// Handler for XObject Images.
+	processor.AddHandler(contentstream.HandlerConditionEnumOperand, "Do",
+		func(op *contentstream.ContentStreamOperation, gs contentstream.GraphicsState,
+			resources *model.PdfPageResources) error {
+			if len(op.Params) < 1 {
+				return fmt.Errorf("Invalid number of params for Do object. op=%v", op)
+			}
+			// XObject.
+			nameObj, ok := core.GetName(op.Params[0])
+			if !ok {
+				return fmt.Errorf("Invalid type for Do object name. op=%v", op)
+			}
+			name := string(*nameObj)
+
+			// Only process each one once.
+			_, has := processedXObjects[name]
+			if has {
+				return nil
+			}
+			processedXObjects[name] = struct{}{}
+
+			// We only process images
+			if _, xtype := resources.GetXObjectByName(*nameObj); xtype != model.XObjectTypeImage {
+				return nil
+			}
+
+			ximg, err := resources.GetXObjectImageByName(*nameObj)
+			if err != nil {
+				return err
+			}
+
+			// We currently only process 1 and 3 component ICC images.
+			cs := ximg.ColorSpace
+			common.Log.Debug("ColorSpace=%+q cpts=%d", cs.String(), cs.GetNumComponents())
+			if !(cs.String() == "ICCBased" && (cs.GetNumComponents() == 3 || cs.GetNumComponents() == 1)) {
+				return nil
+			}
+
+			common.Log.Debug("Converting 3 cpt ICC to RGB")
+			img, err := ximg.ToImage()
+			if err != nil {
+				return fmt.Errorf("transformImagesToSimpleColor: err=%v", err)
+			}
+			rgbImg, err := ximg.ColorSpace.ImageToRGB(*img)
+			if err != nil {
+				return fmt.Errorf("transformImagesToSimpleColor: err=%v", err)
+			}
+			if cs.GetNumComponents() == 1 {
+				rgbColorSpace := model.NewPdfColorspaceDeviceRGB()
+				rgbImg, err = rgbColorSpace.ImageToGray(rgbImg)
+				if err != nil {
+					return fmt.Errorf("transformImagesToSimpleColor: err=%v", err)
+				}
+			}
+			// Update the XObject image.
+			// Use same encoder as input data.  Make sure for DCT filter it is updated to 1 color component.
+			encoder := ximg.Filter
+			if dctEncoder, is := encoder.(*core.DCTEncoder); is {
+				dctEncoder.ColorComponents = cs.GetNumComponents()
+			}
+			ximgRGB, err := model.NewXObjectImageFromImage(&rgbImg, nil, encoder)
+			if err != nil {
+				if err == core.ErrUnsupportedEncodingParameters {
+					// Unsupported encoding parameters, revert to a basic flate encoder without predictor.
+					encoder = core.NewFlateEncoder()
+					panic("will this work")
+				}
+				// Try again, fail if error.
+				ximgRGB, err = model.NewXObjectImageFromImage(&rgbImg, nil, encoder)
+				if err != nil {
+					return fmt.Errorf("transformImagesToSimpleColor: Error creating image err=%v", err)
+				}
+			}
+			// Update the entry.
+			err = resources.SetXObjectImageByName(*nameObj, ximgRGB)
+			if err != nil {
+				return fmt.Errorf("transformImagesToSimpleColor: Failed setting XObject %+q. err=%v",
+					name, err)
+			}
+
+			return nil
+		})
+
+	return processor.Process(resources)
+}
+
+// mb returns the number of megabytes in `size` bytes.
+func mb(size int64) float64 {
+	return float64(size) / 1024.0 / 1024.0
+}
+
+// makeUsage updates flag.Usage to include usage message `msg`.
+func makeUsage(msg string) {
+	usage := flag.Usage
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, msg)
+		usage()
+	}
 }
