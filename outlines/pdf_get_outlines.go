3e04e76ebab22f009f2ac8efd5e0e550b9c3c827
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2020-01-21 22:18:16 +0000

    outline examples

---
+++ outlines/pdf_get_outlines.go
@@ -0,0 +1,80 @@
+/*
+ * Prints PDF page info: Mediabox size and other parameters.
+ * If [page num] is not specified prints out info for all pages.
+ *
+ * Run as: go run pdf_info.go input.pdf [page num]
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/unidoc/unipdf/v3/common"
+	"github.com/unidoc/unipdf/v3/model"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage:  go run pdf_get_outlines.go input.pdf\n")
+		os.Exit(1)
+	}
+
+	inputPath := os.Args[1]
+
+	// Enable debug-level logging.
+	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
+
+	fmt.Printf("Input file: %s\n", inputPath)
+
+	err := getOutlines(inputPath)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func getOutlines(inputPath string) error {
+	f, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	pdfReader, err := model.NewPdfReader(f)
+	if err != nil {
+		return err
+	}
+
+	isEncrypted, err := pdfReader.IsEncrypted()
+	if err != nil {
+		return err
+	}
+
+	// Try decrypting with an empty one.
+	if isEncrypted {
+		auth, err := pdfReader.Decrypt([]byte(""))
+		if err != nil {
+			return err
+		}
+		if !auth {
+			common.Log.Debug("Encrypted - unable to access - update code to specify pass")
+			return nil
+		}
+	}
+
+	outlines, err := pdfReader.GetOutlines()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(outlines, "", "    ")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", data)
+
+	return nil
+}
