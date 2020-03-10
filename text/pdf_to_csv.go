9a78537f150d1594a32cf211489323923a72d29f
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2020-03-10 08:52:06 +0000

    Update pdf_to_csv.go

--- text/pdf_to_csv.go
+++ text/pdf_to_csv.go
@@ -2,8 +2,8 @@
  * PDF to CSV: Example illustrating capability to extract TextMarks from PDF, and grouping together into
  * words, rows and columns for CSV data extraction.
  *
- * Includes debugging capabilities such as outputing a marked up PDF showing bounding boxes of marks,
-	 * words, lines and columns.
+ * Includes debugging capabilities such as outputing a marked up PDF showing bounding boxes of marks
+ * words, lines and columns.
  *
  * Run as: go run pdf_to_csv.go -m all -mf markup.pdf table.pdf table.csv
  * - Outputs debug markup including: marks, words, lines, columns to markup.pdf
