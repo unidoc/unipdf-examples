42c7bd20ae81528cc203cce01da7127db43d15f8
Gunnsteinn Hall <gunnsth@nanitor.com>
2020-12-05 22:31:06 +0000

    temporarily disable example requiring crypto11 dependency

--- build_examples.sh
+++ build_examples.sh
@@ -6,7 +6,9 @@ echo "Building to bin/ folder"
 
 #CGO required to build example relying on crypto11 dependency.
 find . -name "*.go" ! -name "*pkcs11*.go" -print0 | CGO_ENABLED=0 xargs -0 -n1 go build
-find . -name "*pkcs11*.go" -print0 | CGO_ENABLED=1 xargs -0 -n1 go build
+#Temporarily disabled due to module issues associated with crypto11 dependency.
+#TODO: Update crypto11 to newest version.
+#find . -name "*pkcs11*.go" -print0 | CGO_ENABLED=1 xargs -0 -n1 go build
 
 mv pdf_* fdf_* jbig2_* bin/
 
