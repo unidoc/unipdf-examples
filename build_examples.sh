741ea774f9123e99ae64a20d5674d265a5da51f2
3ace <adeanom@yahoo.com>
2021-01-24 22:42:28 +0700

    update build script
    - custom encoder example depend on CGO
    - build directly into `bin` folder

--- build_examples.sh
+++ build_examples.sh
@@ -4,9 +4,6 @@ mkdir -p bin
 
 echo "Building to bin/ folder"
 
-#CGO required to build example relying on crypto11 dependency.
-find . -name "*.go" ! -name "*pkcs11*.go" -print0 | CGO_ENABLED=0 xargs -0 -n1 go build
-find . -name "*pkcs11*.go" -print0 | CGO_ENABLED=1 xargs -0 -n1 go build
-
-mv pdf_* fdf_* jbig2_* bin/
-
+#CGO required to build example relying on crypto11 and imagick dependency.
+find . -name "*.go" ! -name "*pkcs11*.go" ! -name "*custom_encoder*.go" -print0 | CGO_ENABLED=0 xargs -0 -n1 go build -o bin
+find . -name "*pkcs11*.go" -name "*custom_encoder*.go" -print0 | CGO_ENABLED=1 xargs -0 -n1 go build -o bin
