0c5c75b96e8a27e9c837e063fc84acd12f65beea
3ace <adeanom@yahoo.com>
2021-01-27 19:36:11 +0700

    update build setting

--- build_examples.sh
+++ build_examples.sh
@@ -4,6 +4,9 @@ mkdir -p bin
 
 echo "Building to bin/ folder"
 
-#CGO required to build example relying on crypto11 and imagick dependency.
+# CGO required to build example relying on crypto11 and imagick dependency.
 find . -name "*.go" ! -name "*_cgo.go" -print0 | CGO_ENABLED=0 xargs -0 -n1 go build -o bin
-find . -name "*_cgo.go" -print0 | CGO_ENABLED=1 xargs -0 -n1 go build -o bin
+find . -name "*pkcs11*_cgo.go" -print0 | CGO_ENABLED=1 xargs -0 -n1 go build -o bin
+
+# Custom encoder example need another CGO flags to build imagick lib.
+find . -name "*custom_encoder*_cgo.go" -print0 | CGO_ENABLED=1 CGO_CFLAGS_ALLOW='-Xpreprocessor' xargs -0 -n1 go build -o bin
