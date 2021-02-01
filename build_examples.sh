7f8aff7837ca24cc351860dcb920da2539845c7e
3ace <adeanom@yahoo.com>
2021-02-01 12:15:38 +0700

    fix build script and reduce verbosity

--- build_examples.sh
+++ build_examples.sh
@@ -8,7 +8,7 @@ mkdir -p bin
 echo "Building to bin/ folder"
 
 # CGO required to build example relying on crypto11 and imagick dependency.
-find . -name "*.go" ! -name "*_cgo.go" ! -name "lib_*" -print0 | CGO_ENABLED=0 xargs -0 -n1 -I% -t bash -c 'go build % || exit 255'
-find . -name "*_cgo.go" -print0 | CGO_ENABLED=1 CGO_CFLAGS_ALLOW='-Xpreprocessor' xargs -0 -n1 -I%  -t bash -c 'go build % || exit 255'
+find . -name "*.go" ! -name "*_cgo.go" ! -name "lib_*" -print0 | CGO_ENABLED=0 xargs -0 -n1 -I% bash -c 'go build % || exit 255'
+find . -name "*_cgo.go" -print0 | CGO_ENABLED=1 CGO_CFLAGS_ALLOW='-Xpreprocessor' xargs -0 -n1 -I% bash -c 'go build % || exit 255'
 
-mv pdf_* fdf_* jbig_* bin/
+mv pdf_* fdf_* jbig2_* bin/
