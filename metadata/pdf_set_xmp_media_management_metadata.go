08808c934811a1606587c253db1b58faf3de8eef
Jacek Kucharczyk <kucjac@gmail.com>
2021-12-15 15:54:38 +0100

    metadata: fixed missing usage.

--- metadata/pdf_set_xmp_media_management_metadata.go
+++ metadata/pdf_set_xmp_media_management_metadata.go
@@ -24,7 +24,7 @@ func init() {
 func main() {
 	args := os.Args
 	if len(args) < 3 {
-		fmt.Printf(usage, os.Args[0])
+		fmt.Printf( "Usage: %s INPUT_PDF_PATH OUTPUT_PDF_PATH\n", os.Args[0])
 		return
 	}
 	inputPath := args[1]
