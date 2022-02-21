79d58e995c5951932f791a21760ca1b5600523b9
kellemNegasi <kellemnegasi@gmail.com>
2022-02-21 16:12:12 +0300

    added example for text redaction

---
+++ redact/redact_text.go
@@ -0,0 +1,78 @@
+/*
+ * PDF to text: Extract all text for each page of a pdf file.
+ *
+ * Run as: go run pdf_extract_text.go input.pdf
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/model"
+	"github.com/unidoc/unipdf/v3/redactor"
+)
+
+func init() {
+	// Make sure to load your metered License API key prior to using the library.
+	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: go run redact_text.go inputFile.pdf outputFile.pdf \n")
+		os.Exit(1)
+	}
+	inputFile := os.Args[1]
+
+	outputFile := os.Args[2]
+
+	// List of regex patterns and replacement strings
+
+	patterns := []string{
+		// Regex for matching credit card number.
+		`(^|\s+)(\d{4}[ -]\d{4}[ -]\d{4}[ -]\d{4})(?:\s+|$)`,
+		// Regex for matching emails.
+		`[a-zA-Z0-9\.\-+_]+@[a-zA-Z0-9\.\-+_]+\.[a-z]+`,
+	}
+	// Replace the first matches i.e the credit cards with `*` and the emails with `#`.
+	replacements := []string{"*", "#"}
+	terms := []redactor.RedactionTerm{}
+	for i, pattern := range patterns {
+		regexp, err := regexp.Compile(pattern)
+		if err != nil {
+			panic(err)
+		}
+		replacement := replacements[i]
+		redTerm := redactor.RedactionTerm{Pattern: regexp, Replacement: replacement}
+		terms = append(terms, redTerm)
+	}
+	pdfReader, f, err := model.NewPdfReaderFromFile(inputFile, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	// Define RedactionOptions
+	options := redactor.RedactionOptions{Terms: terms}
+	red, err := redactor.New(pdfReader, &options)
+	if err != nil {
+		panic(err)
+	}
+	// Execute redaction on the file.
+	err = red.Redact()
+	if err != nil {
+		panic(err)
+	}
+	// Write the redacted document to outputFile.
+	err = red.WriteToFile(outputFile)
+	if err != nil {
+		panic(err)
+	}
+}
