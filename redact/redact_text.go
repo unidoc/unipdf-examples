1e37136ce4083ba298dc0b349015dda1ac5826d3
kellemNegasi <kellemnegasi@gmail.com>
2022-02-23 18:17:27 +0300

    included license setter in init function

--- redact/redact_text.go
+++ redact/redact_text.go
@@ -18,10 +18,10 @@ import (
 func init() {
 	// Make sure to load your metered License API key prior to using the library.
 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
-	// err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-	// if err != nil {
-	// 	panic(err)
-	// }
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
