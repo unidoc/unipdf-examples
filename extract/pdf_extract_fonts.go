3117b7ec59c3d608a7282068afa8de47edd5f7ce
dikyridhlo <dikyridhlo@gmail.com>
2022-03-14 19:41:23 +0700

    update godoc

--- extract/pdf_extract_fonts.go
+++ extract/pdf_extract_fonts.go
@@ -6,18 +6,19 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/unidoc/unipdf/v3/common/license"
 	"github.com/unidoc/unipdf/v3/extractor"
 	"github.com/unidoc/unipdf/v3/model"
 )
 
-// func init() {
-// 	// Make sure to load your metered License API key prior to using the library.
-// 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
-// 	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func init() {
+	// Make sure to load your metered License API key prior to using the library.
+	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
+}
 func main() {
 	var (
 		outputPath string
@@ -50,7 +51,7 @@ func main() {
 	}
 }
 
-// extractFontToArchive prints out font information from pdf to stdout, can add to archive if has output path.
+// extractFontToArchive prints out font information from pdf to stdout, can add to archive if has output path. it can be used for specific page.
 func extractFontToArchive(inputPath string, outputPath string, page int) error {
 	var (
 		pdfFont           *extractor.PageFonts
