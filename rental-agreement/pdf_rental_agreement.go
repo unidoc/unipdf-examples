0ad7aa31698c1a92c1e19bc541bdbd3ffd3b2f2f
kellem negasi <kellemnegasi@gmail.com>
2023-01-21 15:57:00 +0300

    generate sample document

--- rental-agreement/pdf_rental_agreement.go
+++ rental-agreement/pdf_rental_agreement.go
@@ -1,23 +1,58 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"log"
 	"os"
 
-	"github.com/unidoc/unipdf/v3/common"
-	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/creator"
 )
 
-func init() {
-	// Make sure to load your metered License API key prior to using the library.
-	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io.
-	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-	if err != nil {
-		panic(err)
-	}
+// func init() {
+// 	// Make sure to load your metered License API key prior to using the library.
+// 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io.
+// 	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
-}
+// 	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
+// }
 
 func main() {
+	c := creator.New()
+	c.SetPageMargins(50, 50, 80, 25)
 
+	// Read main content template.
+	mainTpl, err := readTemplate("templates/main.tpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Draw the main template.
+	if err := c.DrawTemplate(mainTpl, nil, nil); err != nil {
+		log.Fatal(err)
+	}
+
+	// Write output file.
+	if err := c.WriteToFile("unipdf-rental-agreement.pdf"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// readTemplate reads the template at the specified file path and returns it
+// as an io.Reader.
+func readTemplate(tplFile string) (io.Reader, error) {
+	file, err := os.Open(tplFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	buf := bytes.NewBuffer(nil)
+	if _, err = io.Copy(buf, file); err != nil {
+		return nil, err
+	}
+
+	return buf, nil
 }
