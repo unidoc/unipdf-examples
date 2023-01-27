c2edfaebb8074399a1a724d40ca07b53c80517df
kellemNegasi <kellemnegasi@gmail.com>
2023-01-27 23:46:05 +0300

    fixed left and right margins

--- templates/rental-agreement/pdf_rental_agreement.go
+++ templates/rental-agreement/pdf_rental_agreement.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/unidoc/unipdf/v3/common"
+	"github.com/unidoc/unipdf/v3/common/license"
 	"github.com/unidoc/unipdf/v3/creator"
 )
 
@@ -27,19 +29,19 @@ type RentalAgreement struct {
 	PetFee                string   `json:"pet_fee_amount"`
 }
 
-// func init() {
-// 	// Make sure to load your metered License API key prior to using the library.
-// 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io.
-// 	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
-// }
+func init() {
+	// Make sure to load your metered License API key prior to using the library.
+	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io.
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
+	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
+}
 
 func main() {
 	c := creator.New()
-	c.SetPageMargins(50, 50, 100, 25)
+	c.SetPageMargins(70, 50, 100, 70)
 	// Read main content template.
 	mainTpl, err := readTemplate("templates/main.tpl")
 	if err != nil {
--- templates/rental-agreement/templates/header.tpl
+++ templates/rental-agreement/templates/header.tpl
@@ -1,7 +1,7 @@
-<table columns="1">
+<table columns="1" margin="20 70 0 70">
 <table-cell indent="0" align="center">
 <division margin="0">
-    <image src="path('templates/res/company.png')" width="200" height="50" margin="0 0 0 0"></image>
+    <image src="path('templates/res/company.png')" width="200" height="50" margin="0 0 5 0"></image>
     <line fit-mode="fill-width" position="relative" thickness= "1.2" margin="0"></line>
     <line fit-mode="fill-width" position="relative" thickness= "1.2" margin="0"></line>
 </division>
