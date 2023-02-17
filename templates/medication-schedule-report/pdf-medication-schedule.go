17459d2bb2a13515d92ba5dda0992611b6affe96
kellemNegasi <kellemnegasi@gmail.com>
2023-02-17 04:01:46 +0300

    uncomment init

--- templates/medication-schedule-report/pdf-medication-schedule.go
+++ templates/medication-schedule-report/pdf-medication-schedule.go
@@ -13,18 +13,20 @@ import (
 	"log"
 	"os"
 
+	"github.com/unidoc/unipdf/v3/common"
+	"github.com/unidoc/unipdf/v3/common/license"
 	"github.com/unidoc/unipdf/v3/creator"
 )
 
-// func init() {
-// 	// Make sure to load your metered License API key prior to using the library.
-// 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io.
-// 	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
-// }
+func init() {
+	// Make sure to load your metered License API key prior to using the library.
+	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io.
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
+	common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
+}
 
 type MedicalData struct {
 	Patient struct {
