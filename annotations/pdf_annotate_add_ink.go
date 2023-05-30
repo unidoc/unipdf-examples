09fd4aadc2262fd81b67d8c8c7144b7c262bda74
Ade Anom A <adeanom@yahoo.com>
2023-05-30 20:53:58 +0700

    use paths instead of points

--- annotations/pdf_annotate_add_ink.go
+++ annotations/pdf_annotate_add_ink.go
@@ -37,33 +37,35 @@ func main() {
 	inputPath := os.Args[1]
 	outputPath := os.Args[2]
 
-	points := [][]draw.Point{
-		[]draw.Point{
-			draw.NewPoint(361.019, 638.415),
-			draw.NewPoint(361.488, 637.946),
-			draw.NewPoint(363.361, 635.605),
-			draw.NewPoint(365.233, 632.796),
-			draw.NewPoint(368.043, 629.518),
-			draw.NewPoint(371.321, 625.772),
-			draw.NewPoint(373.662, 622.494),
-			draw.NewPoint(375.535, 620.621),
-			draw.NewPoint(376.471, 619.216),
-			draw.NewPoint(376.94, 618.28),
-			draw.NewPoint(377.408, 618.28),
-			draw.NewPoint(377.876, 618.748),
-			draw.NewPoint(379.281, 621.089),
-			draw.NewPoint(382.09, 627.177),
-			draw.NewPoint(386.305, 636.073),
-			draw.NewPoint(390.987, 646.843),
-			draw.NewPoint(395.201, 655.272),
-			draw.NewPoint(397.543, 660.891),
-			draw.NewPoint(398.947, 664.168),
-			draw.NewPoint(399.884, 666.041),
-			draw.NewPoint(400.352, 666.51),
+	paths := []draw.Path{
+		{
+			Points: []draw.Point{
+				draw.NewPoint(361.019, 638.415),
+				draw.NewPoint(361.488, 637.946),
+				draw.NewPoint(363.361, 635.605),
+				draw.NewPoint(365.233, 632.796),
+				draw.NewPoint(368.043, 629.518),
+				draw.NewPoint(371.321, 625.772),
+				draw.NewPoint(373.662, 622.494),
+				draw.NewPoint(375.535, 620.621),
+				draw.NewPoint(376.471, 619.216),
+				draw.NewPoint(376.94, 618.28),
+				draw.NewPoint(377.408, 618.28),
+				draw.NewPoint(377.876, 618.748),
+				draw.NewPoint(379.281, 621.089),
+				draw.NewPoint(382.09, 627.177),
+				draw.NewPoint(386.305, 636.073),
+				draw.NewPoint(390.987, 646.843),
+				draw.NewPoint(395.201, 655.272),
+				draw.NewPoint(397.543, 660.891),
+				draw.NewPoint(398.947, 664.168),
+				draw.NewPoint(399.884, 666.041),
+				draw.NewPoint(400.352, 666.51),
+			},
 		},
 	}
 
-	err := annotatePdfAddInk(inputPath, outputPath, points)
+	err := annotatePdfAddInk(inputPath, outputPath, paths)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
@@ -73,7 +75,7 @@ func main() {
 }
 
 // Annotate pdf file.
-func annotatePdfAddInk(inputPath string, outputPath string, points [][]draw.Point) error {
+func annotatePdfAddInk(inputPath string, outputPath string, paths []draw.Path) error {
 	// Read the input pdf file.
 	f, err := os.Open(inputPath)
 	if err != nil {
@@ -91,8 +93,8 @@ func annotatePdfAddInk(inputPath string, outputPath string, points [][]draw.Poin
 	opt := &model.ReaderToWriterOpts{
 		PageProcessCallback: func(pageNum int, page *model.PdfPage) error {
 			inkAnnot, err := annotator.CreateInkAnnotation(annotator.InkAnnotationDef{
-				Points: points,
-				// Color:  model.NewPdfColorDeviceRGB(0.89, 0.13, 0.21),
+				Paths: paths,
+				Color: model.NewPdfColorDeviceRGB(0.89, 0.13, 0.21),
 			})
 			if err != nil {
 				return err
