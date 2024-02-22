eab5e2288625eb9c2b68555563bbd62d6b9eb5af
kellem negasi <kellemnegasi@gmail.com>
2024-02-22 21:28:21 +0300

    remove comment in init function

--- table-info/partition_text.go
+++ table-info/partition_text.go
@@ -11,18 +11,20 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/unidoc/unipdf/v3/common/license"
 	"github.com/unidoc/unipdf/v3/extractor"
 	"github.com/unidoc/unipdf/v3/model"
 )
 
-// func init() {
-// 	// Make sure to load your metered License API key prior to using the library.
-// 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
-// 	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func init() {
+	// Make sure to load your metered License API key prior to using the library.
+	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 
 	if len(os.Args) < 3 {
