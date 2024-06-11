87cca201bf9ce787a7b69efe6837a5227eddb050
kellem negasi <kellemnegasi@gmail.com>
2024-06-11 13:00:41 +0300

    made minor updates and added comments

--- concurrent-processing/concurrent_extraction.go
+++ concurrent-processing/concurrent_extraction.go
@@ -1,3 +1,11 @@
+/*
+ * This example demonstrates how to extract content form multiple documents concurrently
+ * with each document extraction running in its own go routine.
+ * N.B. currently concurrency is supported on a document level which means we can only extract one document per go routine.
+ *
+ * Run as: go run concurrent_extraction.go <input1.pdf> <input2.pdf> <input3.pdf>... <output_dir>
+ */
+
 package main
 
 import (
@@ -23,8 +31,8 @@ func init() {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: go run pdf_extract_text.go input1.pdf input2.pdf input3.pdf ... output_dir\n")
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: go run concurrent_extraction.go input1.pdf input2.pdf input3.pdf... output_dir\n")
 		os.Exit(1)
 	}
 	inputDocuments := []string{}
@@ -34,7 +42,7 @@ func main() {
 		if i == len(args)-1 {
 			outputDir = args[i]
 		} else {
-			inputDocuments[i] = args[i]
+			inputDocuments = append(inputDocuments, args[i])
 		}
 	}
 
@@ -44,41 +52,45 @@ func main() {
 	fmt.Println("time taken for concurrent extraction", duration)
 }
 
+// runConcurrent takes the list of input documents and destination output directory and runs the extraction concurrently.
 func runConcurrent(documents []string, outputDir string) {
 	res := make(chan map[string]string, 3)
 
 	err := concurrentExtraction(documents, res)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error. extraction failed %v.\n", err)
 	}
 	outputPath := outputDir
 	if _, err := os.Stat(outputPath); err != nil {
 		if os.IsNotExist(err) {
 			err := os.Mkdir(outputPath, fs.ModePerm)
 			if err != nil {
-				fmt.Printf("Error. failed to create directory %s", outputPath)
+				fmt.Printf("Error. failed to create directory %s\n", outputPath)
 			}
 		}
 	}
+
 	for i := 0; i < len(documents); i++ {
 		result := <-res
 		for path, content := range result {
 			basename := filepath.Base(path)
 			fileName := strings.TrimSuffix(basename, filepath.Ext(basename)) + ".txt"
-
-			file, err := os.Create(outputPath + fileName)
+			filePath := filepath.Join(outputPath, fileName)
+			file, err := os.Create(filePath)
 			if err != nil {
-				fmt.Printf("Error. failed to create file. %v", err)
+				fmt.Printf("Error. failed to create file. %v\n", err)
 			}
 			_, err = file.WriteString(content)
 			if err != nil {
-				fmt.Printf("Error. failed to write content. %v", err)
+				fmt.Printf("Error. failed to write content. %v\n", err)
 			}
 		}
 	}
 
 }
 
+// concurrentExtraction launches a go routine for each document in `documents` and writes the result of extraction to
+// the channel `res`.
 func concurrentExtraction(documents []string, res chan map[string]string) error {
 
 	for _, docPath := range documents {
@@ -86,7 +98,7 @@ func concurrentExtraction(documents []string, res chan map[string]string) error 
 		go func(path string, res chan map[string]string) {
 			result, err := extractSingleDoc(path)
 			if err != nil {
-				fmt.Printf("Error. Failed to extract file %v due to %v", filePath, err)
+				fmt.Printf("Error. Failed to extract file %v. %v\n", filePath, err)
 			}
 			temp := map[string]string{
 				filePath: result,
@@ -97,6 +109,7 @@ func concurrentExtraction(documents []string, res chan map[string]string) error 
 	return nil
 }
 
+// extractSingleDoc takes a single file specified by the `filePath` and returns the extracted text.
 func extractSingleDoc(filePath string) (string, error) {
 	pdfReader, _, err := model.NewPdfReaderFromFile(filePath, nil)
 	if err != nil {
